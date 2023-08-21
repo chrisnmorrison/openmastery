0d1c695fe084f28b36c081cf4dfc796a8a08829b
Chris Morrison <chrnatmor@gmail.com>
2023-08-21 10:48:55 -0400

    Create index.js

---
+++ src/theme/DocPaginator/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import DocPaginator from '@theme-original/DocPaginator';
+
+export default function DocPaginatorWrapper(props) {
+  return (
+    <>
+      <DocPaginator {...props} />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2524107865408207"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style={{display:"block", marginTop:"20px"}}
+     data-ad-client="ca-pub-2524107865408207"
+     data-ad-slot="3322802416"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    </>
+  );
+}
